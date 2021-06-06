fd3de323603422e0298905a0128beaa8db11ebf9
matiwa <mateuszivan1994@wp.pl>
2021-06-06 10:42:35 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace A_fence_cipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gBSource = new System.Windows.Forms.GroupBox();
+            this.rBText = new System.Windows.Forms.RadioButton();
+            this.rBFile = new System.Windows.Forms.RadioButton();
+            this.gBOperation = new System.Windows.Forms.GroupBox();
+            this.rBD = new System.Windows.Forms.RadioButton();
+            this.rBE = new System.Windows.Forms.RadioButton();
+            this.gBKey = new System.Windows.Forms.GroupBox();
+            this.tBKey = new System.Windows.Forms.TextBox();
+            this.Btndraw = new System.Windows.Forms.Button();
+            this.Btngo = new System.Windows.Forms.Button();
+            this.tBText = new System.Windows.Forms.TextBox();
+            this.tBMessage = new System.Windows.Forms.TextBox();
+            this.gBSource.SuspendLayout();
+            this.gBOperation.SuspendLayout();
+            this.gBKey.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gBSource
+            // 
+            this.gBSource.Controls.Add(this.rBFile);
+            this.gBSource.Controls.Add(this.rBText);
+            this.gBSource.Location = new System.Drawing.Point(13, 13);
+            this.gBSource.Name = "gBSource";
+            this.gBSource.Size = new System.Drawing.Size(69, 75);
+            this.gBSource.TabIndex = 0;
+            this.gBSource.TabStop = false;
+            this.gBSource.Text = "Source";
+            // 
+            // rBText
+            // 
+            this.rBText.AutoSize = true;
+            this.rBText.Checked = true;
+            this.rBText.Location = new System.Drawing.Point(7, 20);
+            this.rBText.Name = "rBText";
+            this.rBText.Size = new System.Drawing.Size(46, 17);
+            this.rBText.TabIndex = 0;
+            this.rBText.TabStop = true;
+            this.rBText.Text = "Text";
+            this.rBText.UseVisualStyleBackColor = true;
+            this.rBText.CheckedChanged += new System.EventHandler(this.SelectSource);
+            // 
+            // rBFile
+            // 
+            this.rBFile.AutoSize = true;
+            this.rBFile.Location = new System.Drawing.Point(7, 43);
+            this.rBFile.Name = "rBFile";
+            this.rBFile.Size = new System.Drawing.Size(41, 17);
+            this.rBFile.TabIndex = 1;
+            this.rBFile.Text = "File";
+            this.rBFile.UseVisualStyleBackColor = true;
+            this.rBFile.CheckedChanged += new System.EventHandler(this.SelectSource);
+            // 
+            // gBOperation
+            // 
+            this.gBOperation.Controls.Add(this.rBD);
+            this.gBOperation.Controls.Add(this.rBE);
+            this.gBOperation.Location = new System.Drawing.Point(12, 94);
+            this.gBOperation.Name = "gBOperation";
+            this.gBOperation.Size = new System.Drawing.Size(74, 75);
+            this.gBOperation.TabIndex = 2;
+            this.gBOperation.TabStop = false;
+            this.gBOperation.Text = "Operations";
+            // 
+            // rBD
+            // 
+            this.rBD.AutoSize = true;
+            this.rBD.Location = new System.Drawing.Point(7, 43);
+            this.rBD.Name = "rBD";
+            this.rBD.Size = new System.Drawing.Size(62, 17);
+            this.rBD.TabIndex = 1;
+            this.rBD.Text = "Decrypt";
+            this.rBD.UseVisualStyleBackColor = true;
+            this.rBD.CheckedChanged += new System.EventHandler(this.SelectOperation);
+            // 
+            // rBE
+            // 
+            this.rBE.AutoSize = true;
+            this.rBE.Checked = true;
+            this.rBE.Location = new System.Drawing.Point(7, 20);
+            this.rBE.Name = "rBE";
+            this.rBE.Size = new System.Drawing.Size(61, 17);
+            this.rBE.TabIndex = 0;
+            this.rBE.TabStop = true;
+            this.rBE.Text = "Encrypt";
+            this.rBE.UseVisualStyleBackColor = true;
+            this.rBE.CheckedChanged += new System.EventHandler(this.SelectOperation);
+            // 
+            // gBKey
+            // 
+            this.gBKey.Controls.Add(this.Btngo);
+            this.gBKey.Controls.Add(this.Btndraw);
+            this.gBKey.Controls.Add(this.tBKey);
+            this.gBKey.Location = new System.Drawing.Point(12, 175);
+            this.gBKey.Name = "gBKey";
+            this.gBKey.Size = new System.Drawing.Size(115, 128);
+            this.gBKey.TabIndex = 3;
+            this.gBKey.TabStop = false;
+            this.gBKey.Text = "Key";
+            // 
+            // tBKey
+            // 
+            this.tBKey.Location = new System.Drawing.Point(7, 20);
+            this.tBKey.Name = "tBKey";
+            this.tBKey.Size = new System.Drawing.Size(100, 20);
+            this.tBKey.TabIndex = 0;
+            // 
+            // Btndraw
+            // 
+            this.Btndraw.Location = new System.Drawing.Point(7, 47);
+            this.Btndraw.Name = "Btndraw";
+            this.Btndraw.Size = new System.Drawing.Size(75, 23);
+            this.Btndraw.TabIndex = 1;
+            this.Btndraw.Text = "Draw";
+            this.Btndraw.UseVisualStyleBackColor = true;
+            this.Btndraw.Click += new System.EventHandler(this.Btndraw_Click);
+            // 
+            // Btngo
+            // 
+            this.Btngo.Location = new System.Drawing.Point(8, 99);
+            this.Btngo.Name = "Btngo";
+            this.Btngo.Size = new System.Drawing.Size(75, 23);
+            this.Btngo.TabIndex = 2;
+            this.Btngo.Text = "Encrypt";
+            this.Btngo.UseVisualStyleBackColor = true;
+            this.Btngo.Click += new System.EventHandler(this.Btngo_Click);
+            // 
+            // tBText
+            // 
+            this.tBText.Location = new System.Drawing.Point(133, 12);
+            this.tBText.Multiline = true;
+            this.tBText.Name = "tBText";
+            this.tBText.Size = new System.Drawing.Size(325, 290);
+            this.tBText.TabIndex = 4;
+            // 
+            // tBMessage
+            // 
+            this.tBMessage.Enabled = false;
+            this.tBMessage.Location = new System.Drawing.Point(464, 12);
+            this.tBMessage.Multiline = true;
+            this.tBMessage.Name = "tBMessage";
+            this.tBMessage.Size = new System.Drawing.Size(325, 290);
+            this.tBMessage.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 315);
+            this.Controls.Add(this.tBMessage);
+            this.Controls.Add(this.tBText);
+            this.Controls.Add(this.gBKey);
+            this.Controls.Add(this.gBOperation);
+            this.Controls.Add(this.gBSource);
+            this.Name = "Form1";
+            this.Text = "A fence cipher";
+            this.gBSource.ResumeLayout(false);
+            this.gBSource.PerformLayout();
+            this.gBOperation.ResumeLayout(false);
+            this.gBOperation.PerformLayout();
+            this.gBKey.ResumeLayout(false);
+            this.gBKey.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gBSource;
+        private System.Windows.Forms.RadioButton rBFile;
+        private System.Windows.Forms.RadioButton rBText;
+        private System.Windows.Forms.GroupBox gBOperation;
+        private System.Windows.Forms.RadioButton rBD;
+        private System.Windows.Forms.RadioButton rBE;
+        private System.Windows.Forms.GroupBox gBKey;
+        private System.Windows.Forms.Button Btngo;
+        private System.Windows.Forms.Button Btndraw;
+        private System.Windows.Forms.TextBox tBKey;
+        private System.Windows.Forms.TextBox tBText;
+        private System.Windows.Forms.TextBox tBMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace A_fence_cipher
+{
+    public partial class Form1 : Form
+    {
+        FileStream file = new FileStream("message.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        int key;
+        string message = "";
+        char[] messagechar;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //draw a key
+            Random r = new Random();
+            tBKey.Text = Convert.ToString(r.Next(int.MinValue, int.MaxValue));
+        }
+
+        private void Btngo_Click(object sender, EventArgs e)
+        {
+            //the key cannot be empty!!!
+            if (tBKey.Text == "") tBKey.Text = "0";
+            key = Convert.ToInt32(tBKey.Text);
+            //loading the message from the text field and saving it to a file
+            if (rBText.Checked == true)
+            {
+                message = tBText.Text;
+                StreamWriter sw = new StreamWriter(file);
+                sw.WriteLine(message);
+                sw.Close();
+            }
+            //loading a message from a file and saving it to the text field
+            else if (rBFile.Checked == true)
+            {
+                StreamReader sr = new StreamReader(file);
+                message = sr.ReadLine();
+                sr.Close();
+                tBText.Text = message;
+            }
+            //the beginning of the algorithm
+            messagechar = message.ToCharArray();
+            //message encryption
+            if (rBE.Checked == true)
+            {
+                char[,] result = new char[key, messagechar.Length];//cryptogram
+                int row = 0;//row number
+                int d = 1;//direction
+                //creating a hurdle
+                for (int j = 0; j < messagechar.Length; j++)
+                {
+                    result[row, j] = messagechar[j];
+                    if (row < key - 1 && d == 1) row++;
+                    else if (row == key - 1)
+                    {
+                        row--;
+                        d = -1;//small
+                    }
+                    else if (row > 0 && d == -1) row--;
+                    else if (row == 0)
+                    {
+                        row++;
+                        d = 1;//grow
+                    }
+                }
+                //writing a ciphertext
+                for (int i = 0; i < key; i++)
+                    for(int j = 0; j < messagechar.Length; j++)
+                        tBMessage.Text += result[i, j].ToString();
+            }
+            //message decryption
+            else if (rBD.Checked == true)
+            {
+                char[,] result = new char[key, messagechar.Length];//cryptogram
+                int d = 1;//direction
+                int row = 0, col = 0;
+                int cursor = 0;
+
+                //creating a hurdle with marker
+                for (int i = 0; i < messagechar.Length; i++)
+                {
+                    if (row == 0) d = 1;//grow
+                    if (row == key - 1) d = -1;//small
+                    //marker to mark the place
+                    result[row,col++] = '*';
+
+                    if (d == 1) row++;
+                    else row--;
+                }
+                //entering a character from the ciphertext to the fence
+                for (int i = 0; i < key; i++) {
+                    for (int j = 0; j < messagechar.Length; j++) {
+                        if (result[i,j] == '*' && cursor < messagechar.Length)
+                            result[i,j] = messagechar[cursor++];
+                    }
+                }
+
+                //writing a message       
+                row = 0;
+                col = 0;
+                for (int i = 0; i < messagechar.Length; i++)
+                {
+                    if (row == 0) d = 1;
+                    if (row == key - 1) d = -1;
+
+                    if (result[row,col] != '*') tBMessage.Text+=result[row,col++].ToString();
+
+                    if (d == 1) row++;
+                    else row--;
+                }
+            }
+            //protection against the next operation
+            gBOperation.Enabled = false;
+            gBSource.Enabled = false;
+            gBKey.Enabled = false;
+            tBText.Enabled = false;
+        }
+
+        //draw a key
+        private void Btndraw_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            tBKey.Text = Convert.ToString(r.Next(int.MinValue, int.MaxValue));
+        }
+
+        ////select a message input method
+        private void SelectSource(object sender, EventArgs e)
+        {
+            if (rBText.Checked == true) tBText.Enabled = true;
+            else if (rBFile.Checked == true) tBText.Enabled = false;
+        }
+
+        //display the selected operation as the button text
+        private void SelectOperation(object sender, EventArgs e)
+        {
+            if (rBE.Checked == true) Btngo.Text = rBE.Text;
+            else if (rBD.Checked == true) Btngo.Text = rBD.Text;
+        }
+    }
+}
